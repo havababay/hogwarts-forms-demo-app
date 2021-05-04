a76b1f59a0b4b33943934e9cb6d6f3e465ef839f
havababay <hava.babay@gmail.com>
2021-05-04 20:21:19 +0300

    Introduce reports list, integrate with angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,18 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
+import { MaterialModule } from './material.module';
 
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
 import { HeaderComponent } from './header/header.component';
+import { ReportsListComponent } from './reports-list/reports-list.component';
 
 @NgModule({
   imports:      [ 
     BrowserModule, 
     FormsModule,
+    MaterialModule,
     RouterModule.forRoot([
-      { path: '', component: HelloComponent }, ]),],
-  declarations: [ AppComponent, HelloComponent, HeaderComponent ],
+      { path: '', component: ReportsListComponent }, ]),],
+  declarations: [ AppComponent, HeaderComponent, ReportsListComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
---
+++ src/assets/data/reports.ts
@@ -0,0 +1,26 @@
+export const reportsMetadata = [
+  {
+    id: 1,
+    name: 'All Gryffindor Students',
+    updated:  new Date("08/01/1982"),
+    updated_by: 'Hava',
+  },
+  {
+    id: 2,
+    name: 'Jesse Labreck',
+    updated: new Date("23/02/1982"),
+    updated_by: 'Hava',
+  },
+  {
+    id: 3,
+    name: 'John Doe',
+    updated: new Date("17/02/1982"),
+    updated_by: 'Hava',
+  },
+  {
+    id: 4,
+    name: 'Donald Trump',
+    updated: new Date("16/03/1982"),
+    updated_by: 'Hava',
+  }
+];
