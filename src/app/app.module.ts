515e40b57a926b849b7ba0c4a8d595148c384752
havababay <hava.babay@gmail.com>
2021-05-05 21:31:52 +0300

    introduce report component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { MaterialModule } from './material.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { ReportsListComponent } from './reports-list/reports-list.component';
+import { ReportComponent } from './report/report.component';
 
 @NgModule({
   imports:      [ 
@@ -14,8 +15,9 @@ import { ReportsListComponent } from './reports-list/reports-list.component';
     ReactiveFormsModule,
     MaterialModule,
     RouterModule.forRoot([
-      { path: '', component: ReportsListComponent }, ]),],
-  declarations: [ AppComponent, HeaderComponent, ReportsListComponent ],
+      { path: '', component: ReportsListComponent }, 
+      { path: 'report/:reportId', component: ReportComponent }]),],
+  declarations: [ AppComponent, HeaderComponent, ReportsListComponent, ReportComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
