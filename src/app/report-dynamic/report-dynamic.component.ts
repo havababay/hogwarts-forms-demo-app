535be7c9841840911c39249f7ac5ff05be2bd1c5
havababay <hava.babay@gmail.com>
2021-05-06 14:22:26 +0300

    add filter control group, and print list

--- src/app/report-dynamic/report-dynamic.component.ts
+++ src/app/report-dynamic/report-dynamic.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder } from '@angular/forms';
 import {MatDialog} from '@angular/material/dialog';
 import { AddFilterDialogComponent } from '../add-filter-dialog/add-filter-dialog.component';
+import {FormBuilder, FormGroup, Validators, FormArray} from '@angular/forms';
 
 @Component({
   selector: 'app-report-dynamic',
@@ -17,17 +17,31 @@ export class ReportDynamicComponent implements OnInit {
 
   reportForm = this.fb.group({
     title: [''],
+    filters: this.fb.array([]),
   });
 
 
   addFilter() : void {
-    console.log('add filter');
+    /*console.log('add filter');
     const dialogRef = this.dialog.open(AddFilterDialogComponent, {
       width: '250px',
     });
 
     dialogRef.afterClosed().subscribe(result => {
       console.log('The dialog was closed');
+    });*/
+    this.filters().push(this.createFilterFormGroup());
+  }
+
+  createFilterFormGroup(): FormGroup {
+    return this.fb.group({
+      name: ['', [Validators.required]],
+      operator: ['', [Validators.required]],
+      value: ['', [Validators.required]],
     });
+  }
+
+  filters() : FormArray {
+    return this.reportForm.get('filters') as FormArray;
   }
 }
