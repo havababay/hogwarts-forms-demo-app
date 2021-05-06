398a3c49b0cf98eb4a2629993edcc263fd008c75
havababay <hava.babay@gmail.com>
2021-05-06 22:46:24 +0300

    Additional fields metadata

--- src/assets/data/filters-metadata.ts
+++ src/assets/data/filters-metadata.ts
@@ -1,23 +1,62 @@
+enum fieldType {
+  Enum,
+  String,
+  Date,
+  Boolean
+}
+
+enum fieldOperator {
+  Equals,
+  Contains,
+  GreaterThan,
+  LowerThan,
+  GreaterThanOrEquals,
+  LowerThanOrEquals,
+  Between
+}
+
 export const filtersMetadata = [
   {
     name: 'house',
     display_name: 'House',
-    type: 'enum',
+    type: fieldType.Enum,
     values : []
   },
   {
     name: 'birth_date',
     display_name: 'Birth Date',
-    type: "date"
+    type: fieldType.Date
   },
   {
     name: 'is_full_blood',
     display_name: 'Is Full Blood',
-    type: "boolean"
+    type: fieldType.Boolean
   },
   {
     name: 'full_name',
     display_name: 'Full Name',
-    type: "string"
+    type: fieldType.String
   }
 ];
+
+export const typeMetadata = new Map<fieldType, fieldOperator[]>([
+  [fieldType.Enum, [fieldOperator.Equals]],
+  [fieldType.Boolean, [fieldOperator.Equals]],
+  [fieldType.String,[fieldOperator.Equals, fieldOperator.Contains]],
+  [fieldType.Date, 
+      [fieldOperator.Equals, 
+       fieldOperator.GreaterThan, 
+       fieldOperator.GreaterThanOrEquals, 
+       fieldOperator.LowerThan, 
+       fieldOperator.LowerThanOrEquals]]
+]);
+
+export const operatorMetadata = new Map<fieldOperator, string>([
+  [fieldOperator.Equals,'Equal to (=)'],
+  [fieldOperator.Contains,''],
+  [fieldOperator.GreaterThan,'Greater than (>)'],
+  [fieldOperator.LowerThan,'Lower than (<)'],
+  [fieldOperator.GreaterThanOrEquals,'Greater than or equal to (>=)'],
+  [fieldOperator.LowerThanOrEquals,'Lower than or equal to (<=)'],
+  [fieldOperator.Between,'Between (>= && <=)']
+]);
