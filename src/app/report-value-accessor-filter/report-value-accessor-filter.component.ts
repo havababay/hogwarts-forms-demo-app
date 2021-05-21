2a9544dee2efeb2f0d7d7a8ac20db2fccea56b78
havababay <hava.babay@gmail.com>
2021-05-21 23:10:48 +0300

    add functionality to value accessor component

--- src/app/report-value-accessor-filter/report-value-accessor-filter.component.ts
+++ src/app/report-value-accessor-filter/report-value-accessor-filter.component.ts
@@ -1,16 +1,50 @@
-import { Component, OnInit } from '@angular/core';
+import { Component} from '@angular/core';
+import { AbstractControl, ControlValueAccessor, FormBuilder, FormGroup, NG_VALIDATORS, NG_VALUE_ACCESSOR, ValidationErrors, Validator, Validators } from '@angular/forms';
 import { fieldOperator, filtersMetadata, operatorMetadata, typeMetadata } from '../../assets/data/filters-metadata';
 
 @Component({
   selector: 'app-report-value-accessor-filter',
   templateUrl: './report-value-accessor-filter.component.html',
-  styleUrls: ['./report-value-accessor-filter.component.css']
+  styleUrls: ['./report-value-accessor-filter.component.css'],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      multi: true,
+      useExisting: ReportValueAccessorFilterComponent
+    },
+    {
+      provide: NG_VALIDATORS,
+      multi: true,
+      useExisting: ReportValueAccessorFilterComponent
+    },
+  ]
 })
-export class ReportValueAccessorFilterComponent implements OnInit {
+export class ReportValueAccessorFilterComponent implements ControlValueAccessor, Validator  {
 
-  constructor() { }
+  constructor(private fb: FormBuilder) { }
 
-  ngOnInit() {
+  filterForm : FormGroup = this.fb.group({
+      name: ['', [Validators.required]],
+      operator: ['', [Validators.required]],
+      value: ['', [Validators.required]],
+    });
+
+  onTouched = () => {};
+
+  validate(control: AbstractControl): ValidationErrors|null {
+     return this.filterForm.valid ? null : {filterForm: 'Problems in filterForm!'};
+  }
+
+  writeValue(obj: any): void {
+    this.filterForm.setValue(obj, {emitEvent: false})
+  }
+
+  registerOnChange(fn: any): void {
+    this.filterForm.valueChanges.subscribe(fn);
+  }
+
+  registerOnTouched(fn: any): void {
+    this.onTouched = fn;
   }
 
   currentOperators(i : number) : fieldOperator[] | undefined{
