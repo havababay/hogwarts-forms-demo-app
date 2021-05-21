5c1430e71121d932c407ef748bb70ef99313b0cd
havababay <hava.babay@gmail.com>
2021-05-21 19:07:37 +0300

    report form group

--- src/app/report-clean-code/src/report-form-group.ts
+++ src/app/report-clean-code/src/report-form-group.ts
@@ -1,5 +1,17 @@
-import { FormBuilder } from "@angular/forms";
+import { FormBuilder, FormGroup } from "@angular/forms";
+import { Report } from "../../../assets/data/report";
 
 export class ReportFormGroup {
-  constructor(private fb : FormBuilder) {}
+  private reportForm : FormGroup;
+
+  constructor(private fb : FormBuilder, initialReport : Report) {
+    this.createReportForm();
+  }
+
+  private createReportForm() {
+    this.reportForm = this.fb.group({
+        title: [''],
+        filters: this.fb.array([]),
+     });;
+  }
 }
