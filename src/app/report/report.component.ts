e3d729aa67d06a6eaa42c5c6aac56c87310872b3
havababay <hava.babay@gmail.com>
2021-05-26 18:09:45 +0300

    hogwarts form in template driven code

--- src/app/report/report.component.ts
+++ src/app/report/report.component.ts
@@ -14,13 +14,16 @@ export class ReportComponent implements OnInit {
   constructor(private route: ActivatedRoute,) { }
 
   ngOnInit() {
+    console.log("hava");
     // First get the product id from the current route.
     const reportIdFromRoute = this.route.snapshot.paramMap.get('reportId');
+        console.log("reportIdFromRoute" + reportIdFromRoute);
+
     // Find the product that correspond with the id provided in route.
     this.report = reportsData.find(report => {
       return report.id === Number(reportIdFromRoute);
     });
 
-    this.reportTitle = this.report.name;
+    this.reportTitle = this.report.title;
   }
 }
